a761c2414334c557b69715b3b0cc95dfb369cf05
Zachary Craig <craig.zac.a@gmail.com>
2024-09-13 15:32:58 -0500

    added comments and my name to authors in ship.py authors
    forgot to add my name to the ship.py authors when I made changes. also added comments to ship.py

--- src/ship.py
+++ src/ship.py
@@ -2,7 +2,7 @@
 Name: ship.py
 Description: This contains our ship class, which holds the size and number of hits on the ship
              We only have two simple functions, to check if a ship is sunk and to add a hit to the ship
-Authors: Carson Treece, ...
+Authors: Carson Treece, Zachary Craig
 Other Sources: ...
 Date Created: 9/9/2024
 Last Modified: 9/9/2024
@@ -12,23 +12,29 @@ class Ship:
     def __init__(self, size, orientation, location):
         self.size = size # record size of the ship
         self.damage = 0 # record number of hits on the ship
-        self.orientation = orientation
-        self.location = location
+        self.orientation = orientation # record orientation of the ship
+        self.location = location # record location of the ship
 
+    # function to return status of the ship
     def is_sunk(self):
-        return self.damage >= self.size # check if sunk by seeing if hits equals size
+        return self.damage >= self.size # returns if sunk by seeing if hits equals (or if it somehow manages to exceed) size
 
+    # function to add damage to its hull
     def hit(self):
         self.damage += 1 # add a hit to the ship
-        
+
+    # function to return the size of the ship
     def getSize(self):
-        return self.size
+        return self.size # returns its size
     
+    # function to return the orientation of the ship
     def getOrientation(self):
-        return self.orientation
+        return self.orientation # returns its orientation
     
+    # function to return the location of the ship
     def getLocation(self):
-        return self.location
+        return self.location # returns its location
     
+    # function to return the amount of damage the ship has taken
     def getDamage(self):
-        return self.damage
+        return self.damage # returns its damage
